ba2763e7a6412449a3e62c367c1d94f483d220bb
Koushik Dutta <koushik.dutta.py@protonmail.com>
2024-08-25 01:10:54 +0530

    stack init and exception list added

--- fnd_messages/stack_texts.py
+++ fnd_messages/stack_texts.py
@@ -1,4 +1,4 @@
 stack_size_error_text = 'stack size parameter should be integer type'
 stack_size_zero_warn = 'Size of stack is zero, still want to continue? '
 stack_safe_mode_flag_error = 'safe_mode flag can only be True or False'
-push_element_is_none = "Please insert a not None element"
+push_element_is_none = 'Please insert a not None element'
